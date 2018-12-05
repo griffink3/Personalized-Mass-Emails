7acd6b66817fa329e13fc9f4e65a4073e0fcd815
Griffin Kao <griffink3@gmail.com>
2018-12-05 16:16:56 -0500

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,24 @@
+Mass Personalized Email Script
+By: Griffin Kao
+
+Instructions:
+To use the script, you need the email provider host address and port (for gmail this will always be "smtp.gmail.com" and "465" respectively), as well as your email address and password.
+So to send email from griffin_kao@brown.edu, I would run "python mass_personalized_email.py smtp.gmail.com 465 griffin_kao@brown.edu password" from terminal or the comand line.
+By default, the script will try to read the personalized info from a file named email_list.csv, the email body from email_text.txt, and the email subject line from subject_text.txt.
+If you want to read from different files, you can specify them: "python mass_personalized_email.py smtp.gmail.com 465 griffin_kao@brown.edu password another_email_list.csv another_email_text.txt another_subject_text.txt".
+The info file must always be a csv and to be safe, you should have your email body text and subject text files be .txt files.
+The csv file should always have a column with the email address with a column header of "email". Each additional column is a piece of personalized info.
+For example, if I want to personalize  name and company name, I could have a csv file that looks like- 
+
+"email,name,company
+griffin_kao@brown.edu,Griffin,Brown University"
+
+And then you would use the column headers surrounded by brackets as the markers in your email text to indicate where the personalized info goes. For example:
+
+"Hi <name>,
+You're company is <company>!
+Bye!"
+
+Note: If your email account is super secure you might get an "SMTPAuthenticationError(code, resp)" when you try running the script. If you're using gmail, you can fix this by going into the security settings of your gmail account (https://myaccount.google.com/u/4/security#signin) and turning on "Allow less secure apps".
+
+Possible Uses: Nigerian Prince scam, sending all your ex's an email saying you have chlamydia, and thanking all the people who came to your barmitzvah so your mom gives you the money.
---
+++ mass_personalized_email.py
@@ -0,0 +1,82 @@
+#! /usr/bin/env python
+"""
+A skeleton python script which reads from an input file,
+writes to an output file and parses command line arguments
+"""
+import argparse
+import csv
+import smtplib
+
+def main():
+    # Parsing for command line arguments 
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument(
+        "host_address", nargs="?", default="address", type=str,
+        help="host address for SMTP")
+    parser.add_argument(
+        "port", nargs="?", default="port", type=str,
+        help="port number for SMTP")
+    parser.add_argument(
+        "sender_email", nargs="?", default="griffin_kao@brown.edu", type=str,
+        help="email address of sender")
+    parser.add_argument(
+        "password", nargs="?", default="password", type=str,
+        help="password")
+    parser.add_argument(
+        "info_file", nargs="?", default="email_list.csv", type=str,
+        help="the csv file containing all the email list info")
+    parser.add_argument(
+        "email_text", nargs="?", default="email_text.txt", type=argparse.FileType("r"),
+        help="text file containing the body of the email")
+    parser.add_argument(
+        "subject_text", nargs="?", default="subject.txt", type=argparse.FileType("r"),
+        help="text file containing the subject of the email")
+    args = parser.parse_args()
+
+    # Reading in the raw email text
+    original_text = args.email_text.read()
+    args.email_text.close()
+    # Reading in the email subject text
+    subject = args.subject_text.read()
+    args.subject_text.close()
+
+    print("Email body: " + original_text)
+    print("Email subject: " + subject)
+
+    raw_input("Press Enter to continue...")
+
+    # Setting up the SMTP server to be able to send emails
+    s = smtplib.SMTP_SSL(host=args.host_address, port=args.port)
+    s.login(args.sender_email, args.password)
+
+    line = 0
+    info = {}
+    with open(args.info_file) as info_file:
+        reader = csv.reader(info_file, delimiter=',')
+        for row in reader:
+            if line == 0:
+                # Setting up the dictionary to store personalized info location from the csv header
+                for i in range(len(row)):
+                    token = "<" + row[i] + ">"
+                    info[token] = i 
+                if "<email>" not in info:
+                    print("Error: Info csv must contain emails")
+                    return
+            else:
+                new = original_text
+                email = row[info["<email>"]] # Getting individual email
+                # Replace tokens with personalized tokens
+                for token in info:
+                    new = new.replace(token, row[info[token]])
+                msg = 'Subject: {}\n\n{}'.format(subject, new) # Creating a new message
+                # Sending the email
+                s.sendmail(args.sender_email, email, msg)
+                print
+                print("EMAIL " + str(line))
+                print("Sent: " + new)
+                print("To: " + email)
+            line += 1
+    s.quit # Quitting the SMTP server
+
+if __name__ == "__main__":
+    main()
